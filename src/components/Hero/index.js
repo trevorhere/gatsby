ae656d875384f1d485d493a33e37144567777ea8
trevorhere <trevoranthonylane@gmail.com>
2018-02-02 15:38:22 -0700

    first

---
+++ src/components/Hero/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Hero = () => (
+  <div
+    style={{
+      display:'flex',
+      background:'url("https://www.walldevil.com/wallpapers/w17/mount-timpanogos.jpg")',
+      height:'100vh',
+      textAlign:'center',
+      alignContent: 'center',
+      alignItems:'center',
+      padding:'0',
+      maring:'0',
+      backgroundSize:'cover',
+      backgroundRepeat:'none'
+
+    }}
+  >
+    <div style={{  margin: '0 auto' }} >
+      <h1 style={{ fontWeight:'100'}} >Test</h1>
+    </div>
+  </div>
+)
+
+export default Hero
---
+++ src/layouts/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import './index.css'
+import './specialized.css'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Gatsby Default Starter"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header />
+    <Hero />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
